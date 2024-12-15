5912e8a5a13bed2c62ae18aec204bc0147441801
Patrick-Gogolin <patrick.gogolin@hotmail.com>
2024-12-15 18:35:00 +0100

    testimonial-section done

--- src/app/main-content/testimonial-section/testimonial-section.component.ts
+++ src/app/main-content/testimonial-section/testimonial-section.component.ts
@@ -18,21 +18,21 @@ export class TestimonialSectionComponent {
      firstCharacterOfFirstName: "V.",
      lastName: "Schuster",
      role: "Team Partner",
-     testimonial: "Michael really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment"
+     testimonial: "Patrick really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment"
     },
     {
       img: "../../../assets/img/testimonials/testimonial_one.png",
       firstCharacterOfFirstName: "M.",
       lastName: "Maus",
       role: "Team Partner",
-      testimonial: "Michi was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a significant contribution to us achieving our goals."
+      testimonial: "Paddy was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a significant contribution to us achieving our goals."
      },
      {
       img: "../../../assets/img/testimonials/testimonial_one.png",
       firstCharacterOfFirstName: "G.",
       lastName: "Gans",
       role: "Team Partner",
-      testimonial: "It was a great pleasure to work with Michael. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm. Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well."
+      testimonial: "It was a great pleasure to work with Patrick. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm. Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well."
      },
   ]
 
