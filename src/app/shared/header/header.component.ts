72542322b5d1c2c4e28707ea8471cf2f4808653b
Patrick-Gogolin <patrick.gogolin@hotmail.com>
2024-12-29 14:09:17 +0100

    new commit

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { BurgerMenuComponent } from './burger-menu/burger-menu.component';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [BurgerMenuComponent, RouterLink],
+  imports: [BurgerMenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
