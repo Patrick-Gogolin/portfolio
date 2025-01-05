87e749caeb5493ec3aa77defa41672361ecaeccd
Patrick-Gogolin <patrick.gogolin@hotmail.com>
2025-01-05 18:32:12 +0100

    new commit

--- src/app/skill-section/skill-section-small-viewports/skill-section-small-viewports.component.ts
+++ src/app/skill-section/skill-section-small-viewports/skill-section-small-viewports.component.ts
@@ -16,35 +16,23 @@ export class SkillSectionSmallViewportsComponent {
   icons: string[] = ['html.svg', 'css.svg', 'javascript.svg', 'typescript.svg', 'angular.svg', 'firebase.svg',
     'git.svg', 'restapi.svg', 'scrum.svg', 'material.svg', 'purple-continue-learning.svg'];
 
-  handleTouchStart(event: TouchEvent): void {
-    console.log('Touch started on target image:', event.target);
-  }
-
   startTouch() {
     this.isTouched = true;
-    console.log(this.isTouched);
   }
 
   endOfTouch() {
     this.isTouched = false;
-    console.log(this.isTouched);
   }
 
   @HostListener('touchstart', ['$event'])
   onTouchStart(event: TouchEvent): void {
     const target = event.target as HTMLImageElement;
     const lastIcon = 'continue-learning.svg';
-    console.log(target.src);
-    console.log(lastIcon);
 
     if (target instanceof HTMLImageElement) {
       if (target.src.includes(lastIcon)) {
         event.preventDefault();
-        console.log('Touch event started, context menu is disabled for the last element');
       }
-    }
-    else {
-      console.log("nicht erkannt")
     }
   }
 }
